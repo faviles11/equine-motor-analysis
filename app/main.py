9b96d6f4460515460c6d06d0d59608e5474db24d
faviles11 <avilesfabian11@gmail.com>
2025-05-14 21:58:30 -0600

    clean commits

---
+++ app/main.py
@@ -0,0 +1,9 @@
+# app/main.py
+
+import streamlit as st
+
+st.set_page_config(page_title="Análisis Motor Equino", layout="centered")
+
+st.title("Plataforma de Análisis Motor Equino")
+st.write("🚀 Esta es una versión inicial de la plataforma.")
+st.write("Próximamente podrás cargar tus archivos y comparar los resultados del análisis veterinario e IA.")
